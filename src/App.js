94f8cd92c8e7fe34763b97488c3de74cad66cc1a
Francois HERSENT <francois.hersent@hiventive.com>
2021-12-27 13:56:22 +0100

    use react-children-utilities

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
+import {useForm, FormProvider} from 'react-hook-form'
+import {deepMap, hasComplexChildren} from "react-children-utilities"
 import './App.css';
 
 
-function Form({ defaultValues, children, onSubmit }) {
-  const methods = useForm({ defaultValues });
-  const { handleSubmit } = methods;
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {React.Children.map(children, child => {
-        return  React.createElement(child.type, {
-              ...{
-                ...child.props,
-                register: methods.register,
-                key: child.props.name
-              }
-            })
-       })}
-    </form>
-  );
-}
-
-function Input({ register, name, ...rest }) {
-  return <input {...register(name)} {...rest} />;
+function Input({ register, name }) {
+  return <input ref={register(name)} />;
 }
 
 const Child = () => {
@@ -32,11 +14,50 @@ const Child = () => {
   )
 }
 
+const test = (children, methods)=> deepMap(children, (child) => {
+  console.log("children", children)
+
+  if (hasComplexChildren(children) === true)
+  test(children, methods)
+
+ return React.createElement(child.type, {
+     ...{
+       ...child.props,
+       register: methods.register,
+       key: child.props.name
+     }
+   })
+})
+
+function Form({ defaultValues, children, onSubmit }) {
+  const methods = useForm({ defaultValues });
+  const { handleSubmit } = methods;
+
+
+  console.log("children1", children)
+
+  return (
+    <FormProvider {...methods}>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {test(children, methods)}
+    </form>
+    </FormProvider>
+  );
+}
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <Form onSubmit={e => console.log(e)}>
+        <div>
+          <Input />
+        </div>
+
         <Child />
         <button type='submit'>submit</button>
       </Form>
